db467fe21538b1fdba0c389a3aaf4c96cc6e0b44
Kaio <kaioigordinizz@hotmail.com>
2022-05-07 19:23:20 -0300

    registrando models

--- costumer/admin.py
+++ costumer/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Cliente, Produto
 
 # Register your models here.
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ["id", "nome"]
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ["id", "nome", "sobrenome"]
