d1c74c56d47eb82eda45325b29b47a2c00b996cb
Kaio <kaioigordinizz@hotmail.com>
2022-05-08 16:50:43 -0300

    criando e configurando as views de produtos

--- costumer/urls.py
+++ costumer/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from costumer.views import ClienteListView, DetalhesView
+from costumer.views import ClienteListView, DetalhesView, ProdutoListView, DetalhesProdutoView
 
 app_name = 'costumer'
 urlpatterns = [
-    path('lista/', ClienteListView.as_view(), name='clientes'),
-    path('lista/<pk>/', DetalhesView.as_view(), name='detalhes'),
+    path('/listagem/cliente/', ClienteListView.as_view(), name='clientes'),
+    path('/listagem/cliente/<pk>/', DetalhesView.as_view(), name='cliente_detalhe'),
+    path('/listagem/produto/', ProdutoListView.as_view(), name='produtos'),
+    path('/listagem/produto/<pk>/', DetalhesProdutoView.as_view(), name='produto_detalhe'),
 ]
--- costumer/views.py
+++ costumer/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from costumer.models import Cliente
+from costumer.models import Cliente, Produto
 
 
 class ClienteListView(ListView):
@@ -11,7 +11,19 @@ class ClienteListView(ListView):
 
 class DetalhesView(DetailView):
     model = Cliente
-    template_name = 'vista_detalhada.html'
-    queryset = Cliente.objects.all()
+    template_name = 'cliente_detalhe.html'
     context_object_name = 'clientes'
+
+
+class ProdutoListView(ListView):
+    model = Produto
+    template_name = 'produtos_lista.html'
+    context_object_name = 'produtos'
+
+
+class DetalhesProdutoView(DetailView):
+    model = Produto
+    template_name = 'produto_detalhe.html'
+    context_object_name = 'produtos'
+
 
